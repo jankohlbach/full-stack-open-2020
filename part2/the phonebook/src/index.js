aa726e33294f8b56b0aa10be75da0c98ec4050f1
Jan Kohlbach <mail@jankohlbach.dev>
2020-08-01 15:53:10 +0200

    exercise 2.6

---
+++ part2/the phonebook/src/index.js
@@ -0,0 +1,40 @@
+import React, {useState} from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    {
+      name: 'Arto Hellas',
+    },
+  ]);
+
+  const [newName, setNewName] = useState('');
+
+  const handleChange = (e) => {
+    setNewName(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setPersons(persons.concat({name: newName}));
+    setNewName('');
+  };
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          name: <input value={newName} onChange={handleChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map((person) => <div>{person.name}</div>)}
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
