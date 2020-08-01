b437f5ba66a7364fcb3fb84399d123a490fa5c77
Jan Kohlbach <mail@jankohlbach.dev>
2020-08-01 16:07:17 +0200

    exercise 2.8

--- part2/the phonebook/src/index.js
+++ part2/the phonebook/src/index.js
@@ -9,9 +9,14 @@ const App = () => {
   ]);
 
   const [newName, setNewName] = useState('');
+  const [newNumber, setNewNumber] = useState('');
 
-  const handleChange = (e) => {
+  const handleNameChange = (e) => {
     setNewName(e.target.value);
+  };
+
+  const handleNumberChange = (e) => {
+    setNewNumber(e.target.value);
   };
 
   const handleSubmit = (e) => {
@@ -20,8 +25,12 @@ const App = () => {
     if(persons.some((person) => person.name === newName)) {
       alert(`${newName} is already added to phonebook`);
     } else {
-      setPersons(persons.concat({name: newName}));
+      setPersons(persons.concat({
+        name: newName,
+        number: newNumber,
+      }));
       setNewName('');
+      setNewNumber('');
     }
   };
 
@@ -30,14 +39,17 @@ const App = () => {
       <h2>Phonebook</h2>
       <form onSubmit={handleSubmit}>
         <div>
-          name: <input value={newName} onChange={handleChange} />
+          name: <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          number: <input value={newNumber} onChange={handleNumberChange} />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map((person) => <div>{person.name}</div>)}
+      {persons.map((person) => <div>{person.name} {person.number}</div>)}
     </div>
   );
 };
