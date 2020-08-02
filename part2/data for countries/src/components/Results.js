2a0ee045b4454be89883436cf39ce5b0ea031866
Jan Kohlbach <mail@jankohlbach.dev>
2020-08-02 13:02:01 +0200

    exercise 2.14

--- part2/data for countries/src/components/Results.js
+++ part2/data for countries/src/components/Results.js
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, {useState} from 'react';
+import axios from 'axios';
 
 const Results = (props) => {
+  const [weather, setWeather] = useState();
+
   const countriesToShow = props.countries.filter((country) => country.name.toLowerCase().indexOf(props.search.toLowerCase()) !== -1);
 
   const handleShowClick = (e) => {
     props.handleShowClick(e.target.dataset.country);
+  };
+
+  const getWeather = (city) => {
+    axios
+      .get(`http://api.weatherstack.com/current?access_key=${process.env.REACT_APP_API_KEY}&query=${city}`)
+      .then((response) => {
+        if(response.data.current.temperature !== weather?.temperature)
+          setWeather(response.data.current);
+      });
   };
 
   const getResults = () => {
@@ -15,16 +27,22 @@ const Results = (props) => {
         <div key={country.alpha3Code}>{country.name}<button data-country={country.name} onClick={handleShowClick}>show</button></div>
       )));
     } else {
+      getWeather(countriesToShow[0].capital);
+
       return (
         <>
           <h1>{countriesToShow[0].name}</h1>
           <div>capital {countriesToShow[0].capital}</div>
           <div>population {countriesToShow[0].population}</div>
-          <h2>languages</h2>
+          <h2>Spoken languages</h2>
           <ul>
             {countriesToShow[0].languages.map((language) => <li key={language.iso639_2}>{language.name}</li>)}
           </ul>
           <img src={countriesToShow[0].flag} alt={`Flag ${countriesToShow[0].name}`} />
+          <h2>Weather in {countriesToShow[0].capital}</h2>
+          <div><b>temperature:</b>{weather?.temperature}</div>
+          <img src={weather?.weather_icons[0]} alt={weather?.weather_descriptions[0]} />
+          <div><b>wind:</b>{`${weather?.wind_speed} mph direction ${weather?.wind_dir}`}</div>
         </>
       );
     }
