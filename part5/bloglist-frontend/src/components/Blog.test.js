7d2753287a611fc6f02bb4d362eda5b00898514f
Jan Kohlbach <mail@jankohlbach.dev>
2020-12-12 16:04:21 +0100

    exercise 5.14

--- part5/bloglist-frontend/src/components/Blog.test.js
+++ part5/bloglist-frontend/src/components/Blog.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import '@testing-library/jest-dom/extend-expect';
-import {render} from '@testing-library/react';
+import {render, fireEvent} from '@testing-library/react';
 
 import Blog from './Blog';
 
@@ -25,5 +25,37 @@ describe('<Blog />', () => {
 
     const likes = component.container.querySelector('.likes');
     expect(likes).not.toBeInTheDocument();
+  });
+
+  test('url and likes are shown when button is clicked', () => {
+    const blog = {
+      title: 'Title of blog',
+      author: 'Jan',
+      url: 'test.com',
+      likes: 42,
+      user: {
+        name: 'Jan',
+        username: 'jan',
+      },
+    };
+
+    const user = {
+      name: 'Jan',
+      username: 'jan',
+    };
+
+    const component = render(
+      <Blog blog={blog} user={user} />
+    );
+
+    const button = component.getByText('view');
+
+    fireEvent.click(button);
+
+    const url = component.container.querySelector('.url');
+    expect(url).toBeInTheDocument();
+
+    const likes = component.container.querySelector('.likes');
+    expect(likes).toBeInTheDocument();
   });
 });
