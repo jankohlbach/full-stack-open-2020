98b7a784260b06f69ea1ee38f0b511856fe0d3af
Jan Kohlbach <mail@jankohlbach.dev>
2020-12-12 20:17:44 +0100

    exercise 5.18

--- part5/bloglist-frontend/cypress/integration/blog_app.spec.js
+++ part5/bloglist-frontend/cypress/integration/blog_app.spec.js
@@ -1,11 +1,37 @@
 describe('Blog app', function() {
   beforeEach(function() {
     cy.request('POST', 'http://localhost:3003/api/testing/reset');
+
+    const user = {
+      name: 'Jan',
+      username: 'jan',
+      password: 'admin',
+    };
+
+    cy.request('POST', 'http://localhost:3003/api/users/', user);
     cy.visit('http://localhost:3000');
   });
 
   it('Login form is shown', function() {
     cy.visit('http://localhost:3000');
     cy.contains('log in to application');
+  });
+
+  describe('Login',function() {
+    it('succeeds with correct credentials', function() {
+      cy.get('input[name="Username"]').type('jan');
+      cy.get('input[name="Password"]').type('admin');
+      cy.get('button[type="submit"]').click();
+
+      cy.contains('Jan logged in');
+    });
+
+    it('fails with wrong credentials', function() {
+      cy.get('input[name="Username"]').type('jahn');
+      cy.get('input[name="Password"]').type('root');
+      cy.get('button[type="submit"]').click();
+
+      cy.contains('Request failed with status code 401');
+    });
   });
 });
